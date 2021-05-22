4c63a7e8ab08552e09047400df3b75a647750558
Brian Phillips <brian.phillips201@gmail.com>
2021-05-21 22:37:52 -0400

    minor refactor for test

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -9,12 +9,10 @@ describe("/requestValidation", function() {
 
     it("should return a valid message", function(done) {
         return req.post('/requestValidation')
-            .send({
-                "address": "mvgMDkBZYfomRMaGKoSHXsSZVgiZszmXzf"
-            })
+            .send({ address })
             .expect(200)
-            .then(response => {
-                expect(response.body).toMatch(/mvgMDkBZYfomRMaGKoSHXsSZVgiZszmXzf:[0-9]+:starRegistry/)
+            .then(res => {
+                expect(res.body).toMatch(/mvgMDkBZYfomRMaGKoSHXsSZVgiZszmXzf:[0-9]+:starRegistry/)
                 done()
             })
             .catch(err => done(err))
